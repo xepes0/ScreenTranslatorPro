3a332e3e8cbfb8b8eb2d87b508781e79484fe7b5
xepes0 <kevin.h.cen@gmail.com>
2026-09-22 17:15:52 +0800

    feat: add Vision OCR manager skeleton

---
+++ ScreenTranslatorPro/Core/OCR/OCRManager.swift
@@ -0,0 +1,12 @@
+import Foundation
+import Vision
+import UIKit
+
+final class OCRManager {
+    
+    func recognize(image: UIImage) async throws -> [OCRResult] {
+        // Vision OCR implementation will be added here.
+        // Uses VNRecognizeTextRequest.
+        return []
+    }
+}
