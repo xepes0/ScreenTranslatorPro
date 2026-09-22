2c159eddce408292734aa349a6caaa64864e927d
xepes0 <kevin.h.cen@gmail.com>
2026-09-22 21:23:37 +0800

    feat: switch shortcut result to custom full-screen preview

--- ScreenTranslatorPro/MVP/Intent.swift
+++ ScreenTranslatorPro/MVP/Intent.swift
@@ -1,12 +1,11 @@
 import AppIntents
 import UniformTypeIdentifiers
-import SwiftUI
 import UIKit
 
 struct TranslateScreenshotIntent: AppIntent {
     static var title: LocalizedStringResource = "翻译截图"
-    static var description = IntentDescription("接收当前截图并翻译，在当前上下文显示翻译结果。")
-    static var openAppWhenRun: Bool = false
+    static var description = IntentDescription("接收当前截图并翻译，然后打开全屏译图预览。")
+    static var openAppWhenRun: Bool = true
 
     @Parameter(
         title: "截图",
@@ -15,57 +14,16 @@ struct TranslateScreenshotIntent: AppIntent {
     )
     var screenshot: IntentFile
 
-    func perform() async throws -> some IntentResult & ReturnsValue<IntentFile> & ShowsSnippetView {
+    func perform() async throws -> some IntentResult {
         let data = screenshot.data
         guard let image = UIImage(data: data) else {
             throw ScreenTranslatorError.invalidImage
         }
 
         let result = try await ScreenTranslationEngine().process(image: image)
-        guard let png = result.image.pngData() else {
-            throw ScreenTranslatorError.invalidImage
-        }
+        try PreviewStore.save(result.image)
 
-        let file = IntentFile(
-            data: png,
-            filename: "ScreenTranslatorPro-Translated.png",
-            type: .png
-        )
-
-        return .result(
-            value: file,
-            view: TranslationResultSnippet(imageData: png)
-        )
-    }
-}
-
-private struct TranslationResultSnippet: View {
-    let imageData: Data
-
-    var body: some View {
-        Group {
-            if let image = UIImage(data: imageData) {
-                GeometryReader { proxy in
-                    ScrollView(.vertical) {
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: proxy.size.width)
-                    }
-                    .scrollIndicators(.visible)
-                    .background(Color.black)
-                }
-                .frame(maxWidth: .infinity)
-                .frame(height: 340)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            } else {
-                ContentUnavailableView(
-                    "无法显示译图",
-                    systemImage: "photo.badge.exclamationmark"
-                )
-            }
-        }
-        .padding(.vertical, 4)
+        return .result()
     }
 }
 
