3a2f3fe21c9b6416c7401a605bd3a063f760875c
xepes0 <kevin.h.cen@gmail.com>
2026-09-22 20:37:08 +0800

    ui: enlarge translated result snippet and allow vertical scrolling

--- ScreenTranslatorPro/MVP/Intent.swift
+++ ScreenTranslatorPro/MVP/Intent.swift
@@ -45,12 +45,15 @@ private struct TranslationResultSnippet: View {
     var body: some View {
         Group {
             if let image = UIImage(data: imageData) {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity, maxHeight: 390)
-                    .background(Color.black)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                ScrollView(.vertical) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                }
+                .frame(maxWidth: .infinity, minHeight: 560, maxHeight: 680)
+                .background(Color.black)
+                .clipShape(RoundedRectangle(cornerRadius: 18))
             } else {
                 ContentUnavailableView(
                     "无法显示译图",
