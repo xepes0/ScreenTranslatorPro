7530d8d96d5efbe278b235bad10684f2bd0803cb
xepes0 <kevin.h.cen@gmail.com>
2026-09-22 20:13:33 +0800

    feat: present translated image as App Intent result snippet

--- ScreenTranslatorPro/MVP/Intent.swift
+++ ScreenTranslatorPro/MVP/Intent.swift
@@ -1,11 +1,12 @@
 import AppIntents
 import UniformTypeIdentifiers
+import SwiftUI
 import UIKit
 
 struct TranslateScreenshotIntent: AppIntent {
     static var title: LocalizedStringResource = "翻译截图"
-    static var description = IntentDescription("接收当前截图并翻译，完成后自动打开应用内预览。")
-    static var openAppWhenRun: Bool = true
+    static var description = IntentDescription("接收当前截图并翻译，在当前上下文显示翻译结果。")
+    static var openAppWhenRun: Bool = false
 
     @Parameter(
         title: "截图",
@@ -14,16 +15,50 @@ struct TranslateScreenshotIntent: AppIntent {
     )
     var screenshot: IntentFile
 
-    func perform() async throws -> some IntentResult {
+    func perform() async throws -> some IntentResult & ReturnsValue<IntentFile> & ShowsSnippetView {
         let data = screenshot.data
         guard let image = UIImage(data: data) else {
             throw ScreenTranslatorError.invalidImage
         }
 
         let result = try await ScreenTranslationEngine().process(image: image)
-        try PreviewStore.save(result.image)
+        guard let png = result.image.pngData() else {
+            throw ScreenTranslatorError.invalidImage
+        }
 
-        return .result()
+        let file = IntentFile(
+            data: png,
+            filename: "ScreenTranslatorPro-Translated.png",
+            type: .png
+        )
+
+        return .result(
+            value: file,
+            view: TranslationResultSnippet(imageData: png)
+        )
+    }
+}
+
+private struct TranslationResultSnippet: View {
+    let imageData: Data
+
+    var body: some View {
+        Group {
+            if let image = UIImage(data: imageData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: 390)
+                    .background(Color.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+            } else {
+                ContentUnavailableView(
+                    "无法显示译图",
+                    systemImage: "photo.badge.exclamationmark"
+                )
+            }
+        }
+        .padding(.vertical, 4)
     }
 }
 
